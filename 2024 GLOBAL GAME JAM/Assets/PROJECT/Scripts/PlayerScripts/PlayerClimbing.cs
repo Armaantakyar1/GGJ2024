a0015e967e73370c9bd11ea44f6625ba31752d52
Avinash <avi.g751@gmail.com>
2024-01-27 17:12:23 +0400

    player animations set up

--- 2024 GLOBAL GAME JAM/Assets/PROJECT/Scripts/PlayerScripts/PlayerClimbing.cs
+++ 2024 GLOBAL GAME JAM/Assets/PROJECT/Scripts/PlayerScripts/PlayerClimbing.cs
@@ -16,12 +16,17 @@ public class PlayerClimbing : MonoBehaviour
     [SerializeField] Vector2 desiredPosition;
     [SerializeField] float correctOffset;
     [SerializeField] float wrongOffset;
-    [SerializeField]Animator playerAnimator;
+    
 
     [Tab("Debug Info")]
     [SerializeField] float timer;
     [SerializeField] Vector2 previousDesiredPosition;
     [SerializeField] bool timerReset;
+
+    [Tab("Animation Info")]
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] AnimationClip idleClip;
+    [SerializeField] AnimationClip climbClip;
 
     private void Start()
     {
@@ -29,21 +34,26 @@ public class PlayerClimbing : MonoBehaviour
         desiredPosition = transform.position;
     }
 
+    [ContextMenu("Climb Up Bitch")]
     public void ClimbUp()
     {
         desiredPosition = new Vector2(desiredPosition.x, desiredPosition.y + correctOffset);
+        playerAnimator.Play(climbClip.name);
     }
 
+    [ContextMenu("Get Bonked Bitch")]
     public void ClimbDown()
     {
         desiredPosition = new Vector2(desiredPosition.x, desiredPosition.y - correctOffset);
+        playerAnimator.Play(idleClip.name);
     }
 
     private void Update()
     {
-        if (desiredPosition == previousDesiredPosition && !timerReset) return;
-        else if(desiredPosition != previousDesiredPosition && !timerReset)
-        {timer = 0;timerReset = true;}
+        if (desiredPosition == previousDesiredPosition && !timerReset)
+        { playerAnimator.Play(idleClip.name); return; }
+        else if (desiredPosition != previousDesiredPosition && !timerReset)
+        { timer = 0; timerReset = true; }
 
         if(timer >= duration)
         {
